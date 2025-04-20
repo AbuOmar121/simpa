e4f93fdf1e8ee2b08a33d5edb40d938241b99d2a
AbuOmar121 <abookyy@gmail.com>
2025-04-20 13:38:50 +0300

    20/4/2025

---
+++ lib/pages/pets/pet_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PetsProfile extends StatefulWidget {
+  const PetsProfile({super.key});
+
+  @override
+  State<PetsProfile> createState() => _PetsProfileState();
+}
+
+class _PetsProfileState extends State<PetsProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
